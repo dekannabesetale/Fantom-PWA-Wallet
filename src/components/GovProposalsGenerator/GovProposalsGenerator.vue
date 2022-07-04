7604880cc623c81a0f700d2a657b660237e37237
Pavel Spacil <pavel.spacil@gmail.com>
2022-07-04 14:46:26 +0200

    feat(GovProposalsGenerator): create component

---
+++ src/components/GovProposalsGenerator/GovProposalsGenerator.vue
@@ -0,0 +1,23 @@
+<template>
+    <gov-proposals-generator-form @submit="onSubmit" />
+</template>
+
+<script>
+import GovProposalsGeneratorForm from '@/components/forms/GovProposalsGeneratorForm.vue';
+
+export default {
+    name: 'GovProposalsGenerator',
+
+    components: { GovProposalsGeneratorForm },
+
+    methods: {
+        onSubmit(values) {
+            console.log('GovProposalsGenerator', values);
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@import 'style';
+</style>
